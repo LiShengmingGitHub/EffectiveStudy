18bc3fbfbc83c55da22e918ed4b103217bd17581
shengmingli <shengmingli@creditease.cn>
2018-05-27 10:24:12 +0800

    搭建项目

---
+++ code-section/lsm/study/java/baseinfo/IntegerTest.java
@@ -0,0 +1,10 @@
+package lsm.study.java.baseinfo;
+
+/**
+ * Created by shengmingli on 2018/5/27.
+ */
+public class IntegerTest {
+    public static void main(String[] args){
+        System.out.print("ddd");
+    }
+}
--- code-section/src/lsm/study/java/base/IntegerTest.java
+++
@@ -1,8 +0,0 @@
-package lsm.study.java.base;
-
-/**
- * Created by shengmingli on 2018/5/27.
- */
-public class IntegerTest {
-
-}
